78d236fb5481ddf9fadbaabc0064585c80334714
Andrew Schmidt <andrew.t.schmidt@gmail.com>
2012-05-07 14:08:52 -0400

    Initial Commit

---
+++ ChatBubbles/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChatBubbles
+//
+//  Created by Andrew Schmidt on 2012-05-07.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ChatBubbles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChatBubbles
+//
+//  Created by Andrew Schmidt on 2012-05-07.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
